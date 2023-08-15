0650a8454b8890979a76c341fec57c2594635210
Muthonikiboi <kiboijoye254@gmail.com>
2023-08-15 12:46:11 +0300

    synchronous method

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,15 @@
-const fs=require('fs');//require-loads modules in node js,fs-provides a set of functions for working file system
-
+const fs=require('fs'); //require-loads modules in node js,fs-provides a set of functions for working file system
+/*
 const hello='Hello world';
 console.log(hello);
+*/
+
+//read a file Nodejs
+const data=fs.readFileSync('/home/zaki-dev/Desktop/SKILL PAGE/Jonas/complete-node-bootcamp-master/1-node-farm/starter/txt/input.txt','utf8');
+console.log(data);
+
+//write a file in Node
+const writingFile=`what we know about avocados is: ${data}.File created on ${Date.now()}`;
+fs.writeFileSync('/home/zaki-dev/Desktop/SKILL PAGE/Jonas/complete-node-bootcamp-master/1-node-farm/starter/txt/output.txt',writingFile);
+console.log('New file has been written');
 
